663eb94298184b84c4fb46271f16505c25f6a693
charanreddy001 <charanreddypotham111@gmail.com>
2024-03-24 10:12:02 +0530

    c lang

---
+++ first.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+void main()
+{
+    printf("hello world");
+    int i,c;
+    for(i=1;i<=3;i++){
+        for(c=2;c<=c+3;c++){
+            printf("%d",c);
+        }
+        printf("\n");
+    }
+}
